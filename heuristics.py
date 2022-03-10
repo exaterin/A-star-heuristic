4421e80f789e9f90ae54cdfa315508348341d7b9
Ek Lipina <87270011+exaterine@users.noreply.github.com>
2022-03-10 12:14:02 +0100

    -

---
+++ heuristics.py
@@ -0,0 +1,30 @@
+# TODO: Implement more efficient monotonic heuristic
+#
+# Every function receive coordinates of two grid points returns estimated distance between them.
+# Each argument is a tuple of two or three integer coordinates.
+# See file task.md for description of all grids.
+
+import math
+
+def grid_2D_heuristic(current, destination):
+    return abs(current[0] - destination[0]) + abs(current[1] - destination[1])
+
+def grid_diagonal_2D_heuristic(current, destination):
+    return max(abs(current[0] - destination[0]), abs(current[1] - destination[1]))
+
+def grid_3D_heuristic(current, destination):
+    return abs(current[0] - destination[0]) + abs(current[1] - destination[1]) + abs(current[2] - destination[2])
+
+def grid_face_diagonal_3D_heuristic(current, destination):
+    x = abs(current[0] - destination[0])
+    y = abs(current[1] - destination[1])
+    z = abs(current[2] - destination[2])
+    return math.ceil(max(x, y, z, (x + y + z) / 2))
+
+def grid_all_diagonal_3D_heuristic(current, destination):
+    return max(abs(current[0] - destination[0]), abs(current[1] - destination[1]), abs(current[2] - destination[2]))
+
+def grid_knight_2D_heuristic(current, destination):
+    x = abs(current[0] - destination[0])
+    y = abs(current[1] - destination[1])
+    return math.ceil(max(x / 2, y / 2, (x + y) / 3))
